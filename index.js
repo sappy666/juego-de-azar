c36ac1b907aacb18b9e9e6a9025476ceac26505f
Maca <116607202+sappy666@users.noreply.github.com>
2023-03-17 16:20:03 -0300

    correcciones

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Ingresar un número
-let numero = prompt("Ingresa un número:");
+let numero;
+const btnEnviar = document.getElementById("btn-enviar");  
+const inputNumber = document.getElementById("nombre");
+btnEnviar.addEventListener('click', () => {
+  numero = inputNumber.value;
+  suerte(numero);
+});
 
-// Verificar si el número está en el rango de números favorables
-if ((numero > 5 && numero < 15) || numero == 55 || numero == 70) {
-  // Imprimir mensaje si el número está en el rango
-  console.log("El número ingresado tiene opciones de ganar");
-  alert("El número ingresado tiene opciones de ganar");
-} else {
-  // Imprimir mensaje si el número no está en el rango
-  console.log("El número ingresado no es favorable");
-  alert("El número ingresado no es favorable");
-}
+function suerte(number) {
+  // Verificar si el número está en el rango de números favorables
+  if ((numero > 5 && numero < 15) || numero == 55 || numero == 70) {
+    // Imprimir mensaje si el número está en el rango
+    alert("El número ingresado tiene opciones de ganar");
+  } else {
+    // Imprimir mensaje si el número no está en el rango
+    alert("El número ingresado no es favorable");
+  }
+};
